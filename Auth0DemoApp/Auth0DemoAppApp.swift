d62e21ef3c560be027231b2b52c3aa52ba15c7d3
Yevgeny <yezub@pokerplatform.io>
2022-08-08 12:13:10 +0800

    Initial Commit

---
+++ Auth0DemoApp/Auth0DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auth0DemoAppApp.swift
+//  Auth0DemoApp
+//
+//  Created by Yevgeny Yezub on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Auth0DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
