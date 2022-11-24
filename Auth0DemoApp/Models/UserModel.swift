245d0ab5846d051be9a4466cb1323dfadc052a33
Yevgeny <yezub@pokerplatform.io>
2022-11-24 19:29:49 +0800

    RemovePackage, add pods, refactor errors

--- Auth0DemoApp/Models/UserModel.swift
+++ Auth0DemoApp/Models/UserModel.swift
@@ -6,6 +6,7 @@
 //
 
 import JWTDecode
+import Auth0
 
 struct Auth0User {
     let id: String?
@@ -18,13 +19,15 @@ struct Auth0User {
     init?(from idToken: String) {
         guard let jwt = try? decode(jwt: idToken),
               let id = jwt.subject,
-              let name = jwt["name"].string,
-              let email = jwt["email"].string,
-              let emailVerified = jwt["email_verified"].boolean,
-              let picture = jwt["picture"].string,
-              let updatedAt = jwt["updated_at"].string else {
+              let name = jwt.claim(name: "name").string,
+              let email = jwt.claim(name: "email").string,
+              let emailVerified = jwt.claim(name: "email_verified").boolean,
+              let picture = jwt.claim(name: "picture").string,
+              let updatedAt = jwt.claim(name: "updated_at").string
+        else {
             return nil
         }
+
         self.id = id
         self.name = name
         self.email = email
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Auth0DemoApp' do
+
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Auth0DemoApp
+
+pod 'Auth0'
+
+  target 'Auth0DemoAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
